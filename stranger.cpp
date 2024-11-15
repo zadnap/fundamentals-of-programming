7eb8dbcaa9534186e1559422b9ac550c0625e682
zadnap <zadnap@users.noreply.github.com>
2024-11-15 08:19:12 +0700

    Finish Exercise 2

---
+++ stranger.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string person1, person2;
+
+    cin >> person1 >> person2;
+
+    cout << "Hello " << person1 << " and " << person2 << "." << endl;
+
+    return 0;
+}
