22f2c15959bf1d33757e3532fb05f350a5e6b698
zadnap <zadnap@users.noreply.github.com>
2024-11-15 08:16:07 +0700

    Finish Exercise 2

---
+++ four-season.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Spring" << endl;
+    cout << "Summer" << endl;
+    cout << "Autumn" << endl;
+    cout << "Winter" << endl;
+    return 0;
+}
