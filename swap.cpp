d148430083466697f191c6eb803fcf64faaa91d8
zadnap <zadnap@users.noreply.github.com>
2024-11-20 16:57:41 +0700

    Finish Swap

---
+++ swap.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a, int &b);
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    swap(a, b);
+
+    cout << a << b;
+
+    return 0;
+}
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
