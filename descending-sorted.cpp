396550278a386a8a237f73934786be276df04d19
zadnap <zadnap@users.noreply.github.com>
2024-12-04 19:41:18 +0700

    Finish Descending Sorted

---
+++ descending-sorted.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    int range = n;
+    while (range > 0)
+    {
+        for (int i = 0; i < range; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+        range--;
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
